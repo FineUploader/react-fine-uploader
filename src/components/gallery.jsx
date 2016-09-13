9f0b9b57796da010c7802006decfa4aec91f3a25
Ray Nicholus <rnicholus@users.noreply.github.com>
2016-09-12 22:20:38 -0500

    feat(gallery.jsx): start of <Gallery /> high-level component #4

---
+++ src/components/gallery.jsx
@@ -0,0 +1,128 @@
+// TODO CSS
+
+import React, { Component, PropTypes } from 'react'
+
+import CancelButton from './cancel-button'
+import DeleteButton from './delete-button'
+import Dropzone from './dropzone'
+import FileInput from './file-input'
+import Filename from './filename'
+import Filesize from './filesize'
+import RetryButton from './retry-button'
+import PauseResumeButton from './pause-resume-button'
+import ProgressBar from './progress-bar'
+import Thumbnail from './thumbnail'
+
+class Gallery extends Component {
+    static propTypes = {
+        className: PropTypes.string,
+        uploader: PropTypes.object.isRequired
+    };
+
+    static defaultProps = {
+        className: '',
+        'dropzone-content': <span>Drop Files Here</span>,
+        'dropzone-multiple': true
+    }
+
+    constructor() {
+        super()
+
+        this.state = {
+            submittedFiles: []
+        }
+    }
+
+    componentDidMount() {
+        this.props.uploader.on('statusChange', (id, oldStatus, newStatus) => {
+            if (newStatus === 'submitted') {
+                const submittedFiles = this.state.submittedFiles
+
+                submittedFiles.push(id)
+                this.setState({ submittedFiles })
+            }
+            else if (isFileGone(newStatus)) {
+                const submittedFiles = this.state.submittedFiles
+                const indexToRemove = submittedFiles.indexOf(id)
+
+                submittedFiles.splice(indexToRemove, 1)
+                this.setState({ submittedFiles })
+            }
+        })
+    }
+
+    render() {
+        const dropzoneProps = getComponentProps('dropzone', this.props)
+        const uploader = this.props.uploader
+
+        return (
+            <MaybeDropzone uploader={ uploader } { ...dropzoneProps }>
+                <FileInputComponent uploader={ uploader }/>
+                <ProgressBar uploader={ uploader } />
+                {
+                    this.state.submittedFiles.map(id => (
+                        <div key={ id } style={ { display: 'inline-block' } }>
+                            <ProgressBar id={ id } uploader={ uploader } />
+                            <Thumbnail id={ id } uploader={ uploader } />
+                            <div>
+                                <Filename id={ id } uploader={ uploader } />
+                            </div>
+                            <Filesize id={ id } uploader={ uploader } />
+                            <CancelButton id={ id } uploader={ uploader } />
+                            <RetryButton id={ id } uploader={ uploader } />
+                            <DeleteButton id={ id } uploader={ uploader } />
+                            <PauseResumeButton id={ id } uploader={ uploader } />
+                        </div>
+                    ))
+                }
+            </MaybeDropzone>
+        )
+    }
+}
+
+const MaybeDropzone = ({ children, content, uploader, ...props }) => {
+    const { disabled, ...dropzoneProps } = props
+
+    if (disabled) {
+        return <span>{ children }</span>
+    }
+
+    return (
+        <Dropzone uploader={ uploader }
+                  { ...dropzoneProps }
+        >
+            { content }
+            { children }
+        </Dropzone>
+    )
+}
+
+const FileInputComponent = ({ uploader }) => {
+    return (
+        <FileInput multiple uploader={ uploader }>
+            <button>Select Files</button>
+        </FileInput>
+    )
+}
+
+const getComponentProps = (componentName, allProps) => {
+    const componentProps = {}
+
+    Object.keys(allProps).forEach(propName => {
+        if (propName.indexOf(componentName + '-') === 0) {
+            const componentPropName = propName.substr(componentName.length + 1)
+            componentProps[componentPropName] = allProps[propName]
+        }
+    })
+
+    return componentProps
+}
+
+const isFileGone = status => {
+    return [
+            'canceled',
+            'deleted',
+        ].indexOf(status) >= 0
+}
+
+export default Gallery
--- src/test/manual/tester.jsx
+++ src/test/manual/tester.jsx
@@ -1,21 +1,7 @@
-/**
- * TODO Less ugly test page (probably can wait until high-level components are functional,
- * or this can be used to build up to the high-level component instead.
- */
-
 import React, { Component } from 'react'
 
-import CancelButton from 'src/components/cancel-button'
-import DeleteButton from 'src/components/delete-button'
-import Dropzone from 'src/components/dropzone'
-import FileInput from 'src/components/file-input'
-import Filename from 'src/components/filename'
-import Filesize from 'src/components/filesize'
 import FineUploader from 'src/wrappers/traditional'
-import RetryButton from 'src/components/retry-button'
-import PauseResumeButton from 'src/components/pause-resume-button'
-import ProgressBar from 'src/components/progress-bar'
-import Thumbnail from 'src/components/thumbnail'
+import Gallery from 'src/components/gallery'
 
 const uploader = new FineUploader({
     options: {
@@ -37,73 +23,13 @@ const uploader = new FineUploader({
 })
 
 class Tester extends Component {
-    constructor() {
-        super()
-
-        this.state = {
-            submittedFiles: []
-        }
-    }
-
-    componentDidMount() {
-        uploader.on('statusChange', (id, oldStatus, newStatus) => {
-            if (newStatus === 'submitted') {
-                const submittedFiles = this.state.submittedFiles
-
-                submittedFiles.push(id)
-                this.setState({ submittedFiles })
-            }
-            else if (isFileGone(newStatus)) {
-                const submittedFiles = this.state.submittedFiles
-                const indexToRemove = submittedFiles.indexOf(id)
-
-                submittedFiles.splice(indexToRemove, 1)
-                this.setState({ submittedFiles })
-            }
-        })
-    }
-
     render() {
         return (
-            <Dropzone multiple
-                      style={ { border: '1px dotted', minHeight: 400, width: 700} }
-                      uploader={ uploader }
-            >
-                <span>Drop Files Here</span>
-                <FileInputComponent />
-                <ProgressBar uploader={ uploader } />
-                {
-                    this.state.submittedFiles.map(id => (
-                        <div key={ id } style={ { display: 'inline-block' } }>
-                            <ProgressBar id={ id } uploader={ uploader } />
-                            <Thumbnail id={ id } uploader={ uploader } />
-                            <div>
-                                <Filename id={ id } uploader={ uploader } />
-                            </div>
-                            <Filesize id={ id } uploader={ uploader } />
-                            <CancelButton id={ id } uploader={ uploader } />
-                            <RetryButton id={ id } uploader={ uploader } />
-                            <DeleteButton id={ id } uploader={ uploader } />
-                            <PauseResumeButton id={ id } uploader={ uploader } />
-                        </div>
-                    ))
-                }
-            </Dropzone>
+            <Gallery dropzone-style={ { border: '1px dotted', minHeight: 400, width: 700} }
+                     uploader={ uploader }
+            />
         )
     }
-}
-
-const FileInputComponent = () => (
-    <FileInput multiple uploader={ uploader }>
-        <button>Select Files</button>
-    </FileInput>
-)
-
-const isFileGone = status => {
-    return [
-        'canceled',
-        'deleted',
-    ].indexOf(status) >= 0
 }
 
 export default Tester
