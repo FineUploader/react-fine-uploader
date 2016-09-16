9a178e57b2dee378e1b4f31bd35ba45f8d3a8a60
Ray Nicholus <rnicholus@users.noreply.github.com>
2016-09-15 23:06:47 -0500

    feat(gallery/index.jsx): finish rendering all components #4

---
+++ src/components/gallery/index.jsx
@@ -0,0 +1,184 @@
+// TODO CSS
+
+import React, { Component, PropTypes } from 'react'
+
+import CancelButton from '../cancel-button'
+import DeleteButton from '../delete-button'
+import Dropzone from '../dropzone'
+import FileInput from '../file-input'
+import Filename from '../filename'
+import Filesize from '../filesize'
+import RetryButton from '../retry-button'
+import PauseResumeButton from '../pause-resume-button'
+import ProgressBar from '../progress-bar'
+import Thumbnail from '../thumbnail'
+
+import './gallery.css'
+
+class Gallery extends Component {
+    static propTypes = {
+        className: PropTypes.string,
+        uploader: PropTypes.object.isRequired
+    };
+
+    static defaultProps = {
+        className: '',
+        'dropzone-content': <span>Drop Files Here</span>,
+        'dropzone-multiple': true,
+        'fileInput-children': <button>Select Files</button>,
+        'fileInput-multiple': true
+    }
+
+    constructor() {
+        super()
+
+        this.state = {
+            submittedFiles: []
+        }
+    }
+
+    componentDidMount() {
+        this.props.uploader.on('statusChange', (id, oldStatus, newStatus) => {
+            if (newStatus === 'submitted') {
+                const submittedFiles = this.state.submittedFiles
+
+                submittedFiles.push(id)
+                this.setState({ submittedFiles })
+            }
+            else if (isFileGone(newStatus)) {
+                const submittedFiles = this.state.submittedFiles
+                const indexToRemove = submittedFiles.indexOf(id)
+
+                submittedFiles.splice(indexToRemove, 1)
+                this.setState({ submittedFiles })
+            }
+        })
+    }
+
+    render() {
+        const cancelButtonProps = getComponentProps('cancelButton', this.props)
+        const dropzoneProps = getComponentProps('dropzone', this.props)
+        const fileInputProps = getComponentProps('fileInput', this.props)
+        const filenameProps = getComponentProps('filename', this.props)
+        const filesizeProps = getComponentProps('filesize', this.props)
+        const progressBarProps = getComponentProps('progressBar', this.props)
+        const retryButtonProps = getComponentProps('retryButton', this.props)
+        const thumbnailProps = getComponentProps('thumbnail', this.props)
+        const uploader = this.props.uploader
+
+        const chunkingEnabled = uploader.options.chunking && uploader.options.chunking.enabled
+        const deleteEnabled = uploader.options.deleteFile && uploader.options.deleteFile.enabled
+        const deleteButtonProps = deleteEnabled && getComponentProps('deleteFileButton', this.props)
+        const pauseResumeButtonProps = chunkingEnabled && getComponentProps('pauseResumeButton', this.props)
+
+        return (
+            <MaybeDropzone uploader={ uploader } { ...dropzoneProps }>
+                {
+                    !fileInputProps.disabled &&
+                        <FileInputComponent uploader={ uploader } { ...fileInputProps }/>
+                }
+                <ProgressBar uploader={ uploader } { ...progressBarProps } />
+                {
+                    this.state.submittedFiles.map(id => (
+                        <div key={ id }
+                             className='react-fine-uploader-gallery-files'
+                        >
+                            <ProgressBar id={ id }
+                                         uploader={ uploader }
+                                         { ...progressBarProps }
+                            />
+                            <Thumbnail id={ id }
+                                       uploader={ uploader }
+                                       { ...thumbnailProps }
+                            />
+                            <div>
+                                <Filename id={ id }
+                                          uploader={ uploader }
+                                          { ...filenameProps }
+                                />
+                            </div>
+                            <Filesize id={ id }
+                                      uploader={ uploader }
+                                      { ...filesizeProps }
+                            />
+                            <CancelButton id={ id }
+                                          uploader={ uploader }
+                                          { ...cancelButtonProps }
+                            />
+                            <RetryButton id={ id }
+                                         uploader={ uploader }
+                                         { ...retryButtonProps }
+                            />
+                            {
+                                deleteEnabled &&
+                                    <DeleteButton id={ id }
+                                                  uploader={ uploader }
+                                                  { ...deleteButtonProps }
+                                    />
+                            }
+                            {
+                                chunkingEnabled &&
+                                    <PauseResumeButton id={ id }
+                                                       uploader={ uploader }
+                                                       { ...pauseResumeButtonProps }
+                                    />
+                            }
+                        </div>
+                    ))
+                }
+            </MaybeDropzone>
+        )
+    }
+}
+
+const MaybeDropzone = ({ children, content, uploader, ...props }) => {
+    const { disabled, ...dropzoneProps } = props
+
+    if (disabled) {
+        return <span>{ children }</span>
+    }
+
+    return (
+        <Dropzone uploader={ uploader }
+                  { ...dropzoneProps }
+        >
+            { content }
+            { children }
+        </Dropzone>
+    )
+}
+
+const FileInputComponent = ({ uploader, ...props }) => {
+    const { children, ...fileInputProps } = props
+
+    return (
+        <FileInput multiple
+                   uploader={ uploader }
+                   { ...fileInputProps }
+        >
+            { children }
+        </FileInput>
+    )
+}
+
+const getComponentProps = (componentName, allProps) => {
+    const componentProps = {}
+
+    Object.keys(allProps).forEach(propName => {
+        if (propName.indexOf(componentName + '-') === 0) {
+            const componentPropName = propName.substr(componentName.length + 1)
+            componentProps[componentPropName] = allProps[propName]
+        }
+    })
+
+    return componentProps
+}
+
+const isFileGone = status => {
+    return [
+        'canceled',
+        'deleted',
+    ].indexOf(status) >= 0
+}
+
+export default Gallery
