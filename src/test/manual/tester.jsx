94c4002b3e86a43a7ba1e58a2e6457833e605d9d
Ray Nicholus <rnicholus@users.noreply.github.com>
2016-10-25 20:41:32 -0500

    chore(development): s3 manual test setup #2

--- src/test/manual/tester.jsx
+++ src/test/manual/tester.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
 
-import FineUploader from 'src/wrappers/traditional'
 import Gallery from 'src/components/gallery'
+import S3FineUploader from 'src/wrappers/s3'
+import TraditionalFineUploader from 'src/wrappers/traditional'
 
-const uploader = new FineUploader({
+const traditionalUploader = new TraditionalFineUploader({
     options: {
         chunking: {
             enabled: true
@@ -22,10 +23,45 @@ const uploader = new FineUploader({
     }
 })
 
+const s3Uploader = new S3FineUploader({
+    options: {
+        chunking: {
+            enabled: true,
+            concurrent: {
+                enabled: true
+            }
+        },
+        debug: true,
+        deleteFile: {
+            enabled: true,
+            endpoint: "/vendor/fineuploader/php-s3-server/endpoint.php"
+        },
+        request: {
+            endpoint: "http://fineuploadertest.s3.amazonaws.com",
+            accessKey: "AKIAIXVR6TANOGNBGANQ"
+        },
+        retry: {
+            enableAuto: true
+        },
+        signature: {
+            endpoint: "/vendor/fineuploader/php-s3-server/endpoint.php"
+        },
+        uploadSuccess: {
+            endpoint: "/vendor/fineuploader/php-s3-server/endpoint.php?success"
+        }
+    }
+})
+
 class Tester extends Component {
     render() {
         return (
-            <Gallery uploader={ uploader } />
+            <div>
+                <div>Traditional</div>
+                <Gallery uploader={ traditionalUploader } />
+
+                <div>S3</div>
+                <Gallery uploader={ s3Uploader } />
+            </div>
         )
     }
 }
