a61ce84772366050d43804856aca1ca3641df563
rakeshmotghare <rakesh.motghare@gmail.com>
2017-09-07 10:19:16 -0700

    Create index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+const FineUploaderTraditional = require('fine-uploader-wrappers');
+
+interface FileInputProps {
+    uploader: FineUploaderTraditional;
+}
+
+export default class FileInput extends React.Component<FileInputProps>{
+}
