d2c6be0587a810fcb210686d25ea10e74d7f2aff
Alberto Marengo <albertomarengo@yahoo.com>
2018-07-26 12:10:59 +0200

    cursor pointer fix

--- src/file-input/styleable-element.jsx
+++ src/file-input/styleable-element.jsx
@@ -1,21 +1,20 @@
 import React from 'react'
 
 const containerStyle = {
-    display: 'inline-block',
-    position: 'relative'
+  position: 'relative',
+  overflow: 'hidden',
+  direction: 'ltr',
 }
 
 const inputStyle = {
-    bottom: 0,
     height: '100%',
-    left: 0,
     margin: 0,
     opacity: 0,
     padding: 0,
     position: 'absolute',
     right: 0,
     top: 0,
-    width: '100%'
+    height: '100%'
 }
 
 const StyleableFileInput = ({ children, className, onChange, ...params }) => (
